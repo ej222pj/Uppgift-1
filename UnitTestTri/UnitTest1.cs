804730083cc42059fa0f24237d33df1c45f63ffe
ej222pj <ej222pj@student.lnu.se>
2013-11-21 11:29:44 +0100

    Fixat fler tester för ex Arrayer

--- UnitTestTri/UnitTest1.cs
+++ UnitTestTri/UnitTest1.cs
@@ -5,48 +5,60 @@ namespace UnitTestTri
 {
     [TestClass]
     public class TriangleUnitTest
-    {
-        [TestMethod]//Ska lyckas
-        public void isScalene()
+    {   //Ska lyckas
+        //Scalene
+        [TestMethod]
+        public void isScaleneTrue()
         {
             Triangle tri = new Triangle(1.132, 1.132, 1.132);
             Assert.IsTrue(tri.isScalene());
         }
         [TestMethod]
-        public void isIsosceles()
+        public void isScaleneFalse()
+        {
+            Triangle tri = new Triangle(1, 1, 2);
+            Assert.IsFalse(tri.isScalene());
+        }
+        //Isosceles
+        [TestMethod]
+        public void isIsoscelesTrue()
         {
             Triangle tri = new Triangle(1.0, 1.4142135623730951, 1.0);
             Assert.IsTrue(tri.isIsosceles());
         }
         [TestMethod]
-        public void isEquilateral()
+        public void isIsoscelesFalse()
+        {
+            Triangle tri = new Triangle(1.0, 1.0, 1.0);
+            Assert.IsFalse(tri.isIsosceles());
+        }
+        //Equilateral
+        [TestMethod]
+        public void isEquilateralTrue()
         {
             Triangle tri = new Triangle(1.1, 1.11, 1.5627219842313604);
             Assert.IsTrue(tri.isEquilateral());
-        }//Nedanstående tester ska misslyckas
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void LessThanThreePoints()
-        {
-            new Triangle(new[] { new Point(0, 0), new Point(1, 1) });
         }
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void MoreThanThreePoints()
+        public void isEquilateralFalse()
         {
-            new Triangle(new[] { new Point(0, 0), new Point(1, 1), new Point(2, 2), new Point(3, 3) });
+            Triangle tri = new Triangle(1.1, 1.11, 1.1);
+            Assert.IsFalse(tri.isEquilateral());
         }
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void TwoIdenticalPoints()
+        public void TestFullArray()
         {
-            new Triangle(new[] { new Point(0, 0), new Point(0, 0), new Point(1, 1) });
+            new Triangle(new Point[] { new Point(3, 3), new Point(4, 4), new Point(5, 5) });
         }
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void EmptyArray()
+        public void TestThreePointConst()
         {
-            new Triangle(new Point[] { });
+            new Triangle(new Point(3, 3), new Point(4, 4), new Point(5, 5));
         }
+        //[TestMethod]
+        //public void TestLoopConst()
+        //{
+        //    new Triangle(, 4);
+        //}
     }
 }
